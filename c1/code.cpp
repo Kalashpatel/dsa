f2024619cdc0c134b33686e999b91551e84c8020
Kalashpatel <kalashpatel606@gmail.com>
2025-05-18 19:38:56 +0530

    loop

--- c1/code.cpp
+++ c1/code.cpp
@@ -5,4 +5,22 @@ int main(){
 
 cout << "Hello, World!\n";
 
+bool isTrue = true;
+cout << "The value of isTrue is: " << isTrue << endl;
+
+char grade = 'A';
+int value = grade;
+cout << value << endl;
+
+double pi = 3.141;
+int piInt = (int)pi;
+cout << "The integer value of pi is: " << piInt << endl;
+
+int age;
+cout << "Enter your age: ";
+cin >> age;
+cout << "You entered: " << age << endl;
+
+
+return 0;
 }
---
+++ l2/code.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+
+    // if(age >= 18){
+    //     cout << "you can vote" << endl;
+    // } else {
+    //     cout << "you can't vote" << endl;
+    // }
+    // int marks;
+    // cout << "Enter your marks: ";
+    // cin >> marks;
+
+    // if(marks >= 90){
+    //     cout << "Grade A" << endl;
+    // } else if(marks >= 80){
+    //     cout << "Grade B" << endl;
+    // } else if(marks >= 70){
+    //     cout << "Grade C" << endl;
+    // } else if(marks >= 60){
+    //     cout << "Grade D" << endl;
+    // } else {
+    //     cout << "Grade F" << endl;
+    // }
+
+    // char ch;
+    // cout << "Enter a character: ";
+    // cin >> ch;
+
+    // if(ch >= 'A' && ch <= 'Z'){
+    //     cout << "Uppercase letter" << endl;
+    // } else if(ch >= 'a' && ch <= 'z'){
+    //     cout << "Lowercase letter" << endl;
+    // } else if(ch >= '0' && ch <= '9'){
+    //     cout << "Digit" << endl;
+    // } else {
+    //     cout << "Special character" << endl;
+    // }
+
+    // int count = 1;
+
+    // while(count <= 10){
+    //     cout << count << " ";
+    //     count++;
+    // }
+
+    // int i;
+    // for(i = 1; i <= 10; i = i + 2){
+        
+    //     cout << i << " ";
+        
+    // }
+
+    // int n = 50;
+    // int sum = 0;
+
+    // for(int i = 1; i <= n; i++){
+        
+    //     sum = sum + i;
+
+    //     if(i == 10){
+    //         break;
+    //     }
+    // }
+    // cout << sum << endl;
+
+    // int n=5;
+    // int sum = 0;
+    // for(int i = 1; i <= n; i++){
+    //     if(i % 2 != 0){
+    //        sum += i; 
+    //     }
+        
+    // }
+    // cout << "Sum of odd numbers from 1 to " << n << " is: " << sum << endl;
+
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    
+    // for(int i =2; i < n; i++){
+
+    //     if(n % i == 0){
+    //         cout << n << " is not a prime number." << endl;
+    //         break;
+    //     }
+    //     else{
+    //         cout << n << " is a prime number." << endl;
+    //         break;
+    //     }
+    // }
+
+    
+
+    return 0;
+}
